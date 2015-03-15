00725ced873009d3d0e7a0b4c12994178b33a343
Rendom <a@vvt.nu>
2015-03-15 10:16:01 +0100

    @c0r73x update

--- zsh/plugins/livestreamer-twitch.zsh
+++ zsh/plugins/livestreamer-twitch.zsh
@@ -1,37 +1,37 @@
 #!/bin/zsh
 # Twitch + Livestreamer <3
-_livestr_get_following(){
-	cat $XDG_CONFIG_HOME/twitch/following
+_livestr_get_following() {
+    if [ -f $XDG_CONFIG_HOME/twitch/following ]; then
+        /bin/cat $XDG_CONFIG_HOME/twitch/following
+    fi
 }
 
-_livestr_get_online_streams(){
-    LIST=""
-    CSLIST=$(_livestr_get_following | tr -s "\n" ",")
-    RESULT=$(curl -s "https://api.twitch.tv/kraken/streams?channel=$CSLIST")
+_livestr() {
+    if (( CURRENT != 2 )); then
+        return 0
+    fi
+
+    local -a LIST
+    local CSLIST=$(_livestr_get_following | tr -s "\n" ",")
+    local RESULT="$(curl -s "https://api.twitch.tv/kraken/streams?channel=$CSLIST")"
+
     for stream in $(_livestr_get_following); do
-        ISONLINE=$(echo $RESULT | grep -i "\"name\":\"$stream\"")
-        if [[ -n $ISONLINE ]]; then
-            LIST="$stream\n$LIST"
+        local ISONLINE="$(echo $RESULT | sed -n "/\"name\":\"$stream\"/p")"
+        if [ ! -z "$ISONLINE" ]; then
+            LIST+=("${stream}:ONLINE")
+        else
+            LIST+=("${stream}:OFFLINE")
         fi
     done
-    echo $LIST
+
+    _describe -t LIST 'streams' LIST
+
+    return 0
 }
 
-livestr(){
-    if [ ! -z "$1" ]; then
-        R=$1
-    else
-        ONLINESTREAMS=$(_livestr_get_online_streams)
-        R=$(echo $ONLINESTREAMS | dmenu -fn "tamsyn:size=6" -p "Twitch" -i)
-    fi
-    [ -z "$R" ] || livestreamer http://twitch.tv/$R best
+twitch() {
+    [ -z "$1" ] || livestreamer http://twitch.tv/$1 best
 
 }
 
-_livestr(){
-	compadd `_livestr_get_online_streams`
-}
-
-
-
-compdef _livestr livestr
+compdef _livestr twitch
