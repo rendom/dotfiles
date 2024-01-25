251a097b64613d66b98e2edad0a3ec1bd08ce251
rendom <a@vvt.nu>
2024-01-25 16:21:30 +0100

    git-blame and lualine

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -54,21 +54,7 @@ require('lazy').setup({
     config = function()
       vim.cmd.colorscheme 'onedark'
     end,
-  },
-
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+  }, 
 
   {
     -- Add indentation guides even on blank lines
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+local git_blame = require('gitblame')
+
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      theme = 'onedark',
+      component_separators = '|',
+      section_separators = '',
+    },
+    sections = {
+      lualine_x = {
+        { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+        'filetype', 'encoding', 'fileformat'
+      },
+    },
+  },
+}
+
