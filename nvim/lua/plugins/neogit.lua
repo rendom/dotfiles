c924b2a9175762d05423ac0cec3f02b3b1eb05ea
rendom <a@vvt.nu>
2024-01-25 11:15:50 +0100

    Neogit

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+  },
+  config = true,
+  init = function()
+
+    vim.cmd([[
+      cnoreabbrev Magit Neogit
+    ]])
+  end
+}
