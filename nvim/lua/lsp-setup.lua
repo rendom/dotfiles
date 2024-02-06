a177dfa68d3100db396aa6c59948cdb9d6687cb3
rendom <a@vvt.nu>
2024-02-06 09:06:26 +0100

    lsp mappings

--- nvim/lua/lsp-setup.lua
+++ nvim/lua/lsp-setup.lua
@@ -31,11 +31,14 @@ local on_attach = function(_, bufnr)
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [d]efinition')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
+
+  nmap('<leader>f', function() vim.lsp.buf.format { async = true } end, 'Format')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
